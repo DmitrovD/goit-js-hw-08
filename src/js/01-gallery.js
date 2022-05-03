46fee618f2849be559c552abd5326339846657ef
DmitrovD <92047119+DmitrovD@users.noreply.github.com>
2022-05-03 22:23:58 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallaryContainer = document.querySelector('.gallery');
+const gallaryImgMarkup = galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    })
+    .join(``);
+
+gallaryContainer.insertAdjacentHTML('beforeend', gallaryImgMarkup);
+
+let gallery = new SimpleLightbox(`.gallery__item a`, {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
